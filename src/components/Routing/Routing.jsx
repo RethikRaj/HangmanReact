d3e5ff2326a5119ab0be7e9c10c35963320d8f0b
RethikRaj <rethikrajrr@gmail.com>
2025-04-04 12:15:05 +0530

    added routing

---
+++ src/components/Routing/Routing.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import AppLayout from "../../Pages/Layout/AppLayout"
+import StartPage from "../../Pages/StartPage/StartPage"
+
+const Routing = () => {
+    return (
+        <Routes>
+            <Route path="/" element={AppLayout}>
+                <Route index element={<StartPage/>} />
+                <Route path="/play" element={<StartPage/>} />
+            </Route>
+        </Routes>
+    )
+}
+
+export default Routing
