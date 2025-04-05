419d5e03ef2aab2db282986db8a362ee34dd99cf
RethikRaj <rethikrajrr@gmail.com>
2025-04-05 11:53:40 +0530

    added hangman component

--- src/Pages/PlayPage/PlayPage.jsx
+++ src/Pages/PlayPage/PlayPage.jsx
@@ -2,17 +2,22 @@ import { useLocation } from "react-router-dom"
 import MaskedInput from "../../components/MaskedInput/MaskedInput";
 import VirtualKeyBoard from "../../components/VirtualKeyboard/VirtualKeyBoard";
 import { useState } from "react";
+import Hangman from "../../components/Hangman/Hangman";
 
 const PlayPage = () => {
     const location = useLocation();
     const secretWord = location.state?.secretWord;
+    const secretWordSet = new Set(secretWord.toUpperCase().split(""));
 
     const [usedLetters, setUsedLetters] = useState([]);
+    const [chance, setChance] = useState(0);
 
     const handleLetterClick = (character)=>{
-        const updatedGuessedLetters = [...usedLetters,character]
-        setUsedLetters(updatedGuessedLetters);
-        console.log(updatedGuessedLetters);
+        if(!secretWordSet.has(character)){
+            setChance((chance)=>chance+1);
+        }
+        const updatedusedLetters = [...usedLetters,character]
+        setUsedLetters(updatedusedLetters);
     }
 
 
@@ -21,6 +26,8 @@ const PlayPage = () => {
             <MaskedInput secretWord={secretWord} usedLetters={usedLetters}/>
 
             <VirtualKeyBoard secretWord={secretWord} usedLetters={usedLetters} onLetterClick={handleLetterClick}/>
+            
+            <Hangman chance={chance}/>
 
         </div>
     )
